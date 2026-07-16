62d697ea425be80942b085e5439b1111a029a40a
Krishna270704 <167600780+Krishna270704@users.noreply.github.com>
2026-07-16 14:30:03 +0530

    Added Student Information Card project

---
+++ Phase-01 Python Foundation/Student Informatin Card/student_information_card.py
@@ -0,0 +1,16 @@
+print("==================Student Information Card==================")
+Name = input("Enter your name:")
+Age = int(input("Enter your age:"))
+College = input("Enter your college name:")
+Branch = input("Enter your branch:")
+CGPA = float(input("Enter your CGPA:"))
+
+print("\n==================Student Card==================")
+
+print("Name:", Name)
+print("Age:", Age)
+print("College:", College)
+print("Branch:", Branch)
+print("CGPA:", CGPA)
+
+print("====================================")
