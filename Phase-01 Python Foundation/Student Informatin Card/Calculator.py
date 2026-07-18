66c87571b94f7935ef38ff975f7f8f3ecb5350ae
Krishna270704 <167600780+Krishna270704@users.noreply.github.com>
2026-07-18 13:33:31 +0530

    Added Calculator project

---
+++ Phase-01 Python Foundation/Student Informatin Card/Calculator.py
@@ -0,0 +1,18 @@
+print("=================SIMPLE CALCULATOR=================\n")
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+print("Addition :", num1 + num2)
+print("Substraction :", num1 - num2)
+print("Multiplication :", num1 * num2)
+print("Division :", num1 / num2)
+print("Modulus :", num1 % num2)
+print("Floor Division :", num1 // num2)
+print("Exponent :", num1 ** num2)
+print()
+print("==============================================")
+
+print(num1 > num2)
+print(num1==num2)
+
+operations = ["+", "-", "*", "/", "%", "//", "**"]
+print("+" in operations)
