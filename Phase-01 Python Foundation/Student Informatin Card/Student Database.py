611c7e088fd539d949101ec4dee5d15c1a81e46e
Krishna270704 <167600780+Krishna270704@users.noreply.github.com>
2026-07-22 15:42:25 +0530

    Student Database

---
+++ Phase-01 Python Foundation/Student Informatin Card/Student Database.py
@@ -0,0 +1,31 @@
+students = [
+    {
+        "name": "Krishna",
+        "age": 21,
+        "branch": "AIML",
+        "cgpa": 7.39,
+        "city": "Delhi"
+    },
+    {
+        "name": "Rahul",
+        "age": 20,
+        "branch": "CSE",
+        "cgpa": 8.10,
+        "city": "Noida"
+    }
+]
+
+print("====================STUDENT DATABASE=================\n")
+print("\nStudent 1\n)")
+print("Name:", students[0]["name"])
+print("Age:", students[0]["age"])           
+print("Branch:", students[0]["branch"])
+print("CGPA:", students[0]["cgpa"])
+print("City:", students[0]["city"])
+
+print("\nStudent 2\n")
+print("Name:", students[1]["name"])
+print("Age:", students[1]["age"])
+print("Branch:", students[1]["branch"])
+print("CGPA:", students[1]["cgpa"])
+print("City:", students[1]["city"])
